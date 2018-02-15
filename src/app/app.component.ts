3ded03b4864193919fc06d6f9d03c5dfe4b36947
fishiflyfishi <oluwayemifisayo@gmail.com>
2018-02-15 11:17:36 +0100

    Cloud Firestore Demo App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,15 @@
 import { Component } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+interface Post {
+  title: string;
+  content: string;
+}
+interface PostId extends Post {
+  id: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +17,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+
+  postsCol: AngularFirestoreCollection<Post>;
+  // posts: Observable<Post[]>;
+  posts: any;
+
+  title: string;
+  content: string;
+
+
+  postDoc: AngularFirestoreDocument<Post>;
+  post: Observable<Post>;
+
+  constructor(private afs: AngularFirestore) {
+
+  }
+
+  // tslint:disable-next-line:use-life-cycle-interface
+  ngOnInit() {
+    this.postsCol = this.afs.collection('posts');
+    // this.postsCol = this.afs.collection('posts', ref => ref.orderBy('title').limit(3));
+    // for where clause
+   // this.postsCol = this.afs.collection('posts', ref => ref.where('title', '==', 'fishi'));
+    // this.posts = this.postsCol.valueChanges();
+    this.posts = this.postsCol.snapshotChanges()
+    .map(actions => {
+      return actions.map(a => {
+        const data = a.payload.doc.data() as Post;
+        const id = a.payload.doc.id;
+        return {id, data};
+      });
+    });
+
+  }
+
+  addPost() {
+    this.afs.collection('posts').add({'title': this.title, 'content': this.content});
+    // this.afs.collection('posts').doc('my-custom-id').set({'title': this.title, 'content': this.content});
+    this.content = '';
+    this.title = '';
+  }
+
+  // tslint:disable-next-line:no-shadowed-variable
+  getPost(postId) {
+    this.postDoc = this.afs.doc('posts/' + postId);
+    this.post = this.postDoc.valueChanges();
+  }
+
+  deletePost(postId) {
+    this.afs.doc('posts/' + postId).delete();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,20 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { FormsModule } from '@angular/forms';
+
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCDr9wwWhG1B-hljIYmDftnIsK9keIi_FM',
+    authDomain: 'firestore-app-e4db1.firebaseapp.com',
+    databaseURL: 'https://firestore-app-e4db1.firebaseio.com',
+    projectId: 'firestore-app-e4db1',
+    storageBucket: 'firestore-app-e4db1.appspot.com',
+    messagingSenderId: '828696021783'
+};
+
 
 
 @NgModule({
@@ -10,7 +24,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
